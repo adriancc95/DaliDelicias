c0f68bf483982f5309782d49bc7160ab65343268
adriancc95 <adrian.castanoc@gmail.com>
2025-10-03 13:30:02 -0500

    laura

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,23 +5,31 @@ import alpinejs from "@astrojs/alpinejs";
 import playformInline from "@playform/inline";
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astropie.netlify.app",
-	base: "/",
-	// trailingSlash: 'always',
-	integrations: [
-		alpinejs(),
-		playformInline({
-			Beasties: true,
-		}),
-		mdx(),
+  site: "https://astropie.netlify.app",
+  base: "/",
+
+  // trailingSlash: 'always',
+  integrations: [
+      alpinejs(),
+      playformInline({
+          Beasties: true,
+      }),
+      mdx(),
 	],
-	output: "static",
-	devToolbar: {
-		enabled: false,
+
+  output: "static",
+
+  devToolbar: {
+      enabled: false,
 	},
-	vite: {
-		plugins: [tailwindcss()],
+
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  adapter: vercel(),
 });
