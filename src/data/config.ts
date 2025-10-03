e9400f037aaa3001f40d7cee2acef28c277021b1
adriancc95 <adrian.castanoc@gmail.com>
2025-10-03 12:48:40 -0500

    First commit

---
+++ src/data/config.ts
@@ -0,0 +1,58 @@
+// Global variables
+export const siteLang = "es-CO";
+export const siteCurrency = "COP";
+export const siteDomain = "dalidelicias.com";
+export const siteName = "DaliDelicias";
+export const siteSlogan = "Delicias Caseras!";
+export const themeColor = "#22c55e"; // Tailwind green-500
+
+export const phone = {
+	href: "tel:+573103828142",
+	label: "(310)3828142",
+};
+
+export const email = {
+	href: "mailto:lauracas422@hotmail.com",
+	label: "lauracas422@hotmail.com",
+};
+
+export const address = {
+	street: "Dg 30 # 34 a sur 52",
+	city: "Medellin",
+	zip: "12345",
+	state: "Milky Way",
+	country: "Space",
+};
+
+export const socialMedia = {
+	facebook: {
+		url: "https://facebook.com",
+		label: "Facebook",
+		icon: "facebook",
+	},
+	twitter: {
+		url: "https://twitter.com",
+		label: "Twitter",
+		icon: "twitter",
+	},
+	instagram: {
+		url: "https://instagram.com",
+		label: "Instagram",
+		icon: "instagram",
+	},
+	youtube: {
+		url: "https://youtube.com",
+		label: "YouTube",
+		icon: "youtube",
+	},
+};
+
+export const hours = {
+	monday: "9:00 - 17:00",
+	tuesday: "9:00 - 17:00",
+	wednesday: "9:00 - 17:00",
+	thursday: "9:00 - 17:00",
+	friday: "9:00 - 17:00",
+	saturday: "9:00 - 17:00",
+	sunday: "Closed",
+};
---
+++ src/data/menus.js
@@ -0,0 +1,60 @@
+export const menuMain = [
+    {
+        name: "Inicio",
+        url: "/",
+        highlighted: false,
+    },
+    {
+        name: "Menu",
+        url: "/menu",
+        highlighted: false,
+    },
+    {
+        name: "Acerca de mi",
+        url: "/about-us",
+        highlighted: false,
+    },
+    {
+        name: "Galeria",
+        url: "/gallery",
+        highlighted: false,
+    },
+    {
+        name: "Contacto",
+        url: "/contact",
+        highlighted: false,
+    },
+    {
+        name: "Reserva",
+        url: "/book",
+        highlighted: true,
+    },
+];
+
+export const footerPrimary = [
+    {
+        title: "Navigatie",
+        items: [
+            { name: "home", url: "/" },
+        ],
+    },
+];
+
+export const footerSecondary = [
+    {
+        title: "support",
+        items: [
+            { name: "pricing", url: "/pricing" },
+        ],
+    },
+];
+
+export const footerTertiary = [
+    {
+        title: "Links",
+        items: [
+            { name: "privacybeleid", url: '/privacy-policy' },
+            { name: "api status", url: "/api-status" },
+        ],
+    },
+];
